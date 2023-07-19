14962fe88feffcce92e4034ddf9b31337ab4bb48
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-07-19 15:46:19 -0700

    upload image

--- articles/public/js/editor.js
+++ articles/public/js/editor.js
@@ -16,3 +16,26 @@ bannerImage.addEventListener('change', () => {
 uploadInput.addEventListener('change', () => {
     uploadImage(uploadInput, "image");
 })
+
+const uploadImage = (uploadFile, uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new FormData();
+        formdata.append('image', file);
+
+        fetch('/upload', {
+            method: 'post',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            if(uploadType == "image"){
+                addImage(data, file.name);
+            } else{
+                bannerPath = `${location.origin}/${data}`;
+                banner.style.backgroundImage = `url("${bannerPath}")`;
+            }
+        })
+    } else{
+        alert("upload Image only");
+    }
+}
