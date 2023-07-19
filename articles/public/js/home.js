4fe4c4202460e873ecd73e140494570b6b6d69a1
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-07-19 15:10:36 -0700

    commit

--- articles/public/js/home.js
+++ articles/public/js/home.js
@@ -39,3 +39,24 @@ const createFeed = (blog) => {
                         <i class="uil uil-ellipsis-h"></i>
                     </span>
                 </div>
+                
+                <!--clicking on the post should open the full article-->	
+                <div class="photo">
+                    <img src="${data.bannerImage}" alt="">
+                    <div class="feed-cover">
+                        <h2>${data.title.substring(0, 100) + '...'}</h2>
+                        <p>${data.article.substring(0, 200) + '...'}</p>
+
+                        <a href="/${blog.id}"><label class="btn colorful-btn" id="indulge" >Indulge <i class="uil uil-angle-right-b"></i></label></a>
+                    </div>
+                </div>
+                <div class="action-buttons">
+                    <div class="interaction-buttons">
+                        <span><i class="uil uil-thumbs-up act"></i></span>
+                        <span><i class="uil uil-comment-alt-dots"></i></span>
+                        <span><i class="uil uil-share-alt"></i></span>
+                    </div>
+                    <div class="bookmark">
+                        <i class="uil uil-bookmark"></i>
+                    </div>
+                </div>
