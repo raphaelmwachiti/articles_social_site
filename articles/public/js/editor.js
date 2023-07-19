31d2dee5e3f4b5f3f12f84bb763d9db01916c7c7
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-07-19 15:45:10 -0700

    commit

---
+++ articles/public/js/editor.js
@@ -0,0 +1,10 @@
+const blogTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article1');
+
+// banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
