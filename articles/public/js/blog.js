817c2cd4c46155474d20348b724a19441c6f2848
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-07-19 15:19:18 -0700

    commit

--- articles/public/js/blog.js
+++ articles/public/js/blog.js
@@ -9,3 +9,18 @@ docRef.get().then((doc) => {
         location.replace("/");  
     }
 })
+
+const setupBlog = (data) => {
+    const banner = document.querySelector('.banner');
+    const blogTitle = document.querySelector('.title');
+    const titleTag = document.querySelector('title');
+    const publish = document.querySelector('.published');
+    
+    banner.style.backgroundImage = `url(${data.bannerImage})`;
+
+    titleTag.innerHTML += blogTitle.innerHTML = data.title;
+    publish.innerHTML += data.publishedAt;
+
+    const article = document.querySelector('.article1');
+    addArticle(article, data.article);
+}
